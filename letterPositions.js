78df7ed5e947c68a3466d1674ff8e7f18c6d1aff
Anton Smirnov <antosha-85@yandex.ru>
2020-01-08 23:04:21 +0000

    Implemented eslint and created (not myself) that crazy letterPositions.js file

---
+++ letterPositions.js
@@ -0,0 +1,27 @@
+const assertArraysEqual = function(arr1, arr2) {
+  return eqArrays(arr1, arr2);
+};
+
+const eqArrays = (arr1, arr2) => {
+  for (i = 0; i < arr1.length; i ++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const letterPositions = function(str) {
+  const results = {};
+  for (let i = 0; i < str.length; i++) {
+    if (results[str[i]]) {
+      results[str[i]].push(i);
+    } else {
+      results[str[i]] = [i];
+    }
+  }
+  return results;
+};
+
+letterPositions('hello');
+console.log(assertArraysEqual(letterPositions("hello").l, [2,3]));
