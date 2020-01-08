1ad019b082cac1670802cfd450bd26824dd2b0a7
Anton Smirnov <antosha-85@yandex.ru>
2020-01-08 19:59:52 +0000

    Created a new countOnly.js file - first Object's task for today, OMG!

---
+++ countOnly.js
@@ -0,0 +1,45 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`:) Assertion Passed: ${actual} === ${expected}`) 
+  : console.log(`Assertion Failed: ${actual} !== ${expected}`)         
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) { 
+      // inside the loop, increment the counter for each item:
+      // set a property with that string key to: the value that was already there (or zero if nothing there) with 1 added to it.
+      if (results[item]) {
+        results[item] += 1;
+        console.log(`adding ${item} again and the count is ${results[item]}`)
+      } else {
+        results[item] = 1;
+        console.log(`adding ${item} first time and the count is ${results[item]}`)
+      }
+    }
+  }
+
+  return results;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });;
+console.log(result1)
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
